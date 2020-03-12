a0b5f917bd2f00367074a8cbb346148029339eb5
George Matos <georgemattos@gmail.com>
2020-03-12 20:22:31 -0300

    importando as frases para o componente Painel (avancei no desenv. por conta propria como forma de exercicio)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
@@ -16,7 +17,8 @@ import { ProgressoComponent } from './progresso/progresso.component'
     ProgressoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  frases: Frase[] = FRASES;
+
+  item: Frase;
+  fraseEng: string;
+  frasePtBr: string;
+  resposta: string;
+
+  constructor() {
+    this.atualizarFrases();
+  }
 
   ngOnInit(): void {
+
+  }
+
+  enviar() {
+
+    if (this.frasePtBr === this.resposta) {
+      alert("Parabéns, você acertou.");
+    } else {
+      alert("Uma pena, você errou.");
+    }
+
+    this.resposta = "";
+    this.atualizarFrases();
+
+  }
+
+  atualizarFrases() {
+    this.item = this.frases[Math.floor(Math.random() * FRASES.length)];
+    this.fraseEng = this.item.fraseEng;
+    this.frasePtBr = this.item.frasePtBr;
   }
 
 }
